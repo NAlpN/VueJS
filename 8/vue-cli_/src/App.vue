589775e1f7a3727c1ab290a76755b549a9810b5b
Alperen <138169703+NAlpN@users.noreply.github.com>
2024-03-22 10:52:42 +0300

    VueCLI ile components uygulaması

---
+++ 8/vue-cli_/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Title :titleText="text" :text="text2" />
+    <h3>App.vue</h3>
+    <Button @clean="cleanInput"/>
+    <input type="text" ref="inputText">
+  </div>
+</template>
+
+<script>
+import title from './components/Title.vue'
+import Button from './components/Button.vue'
+export default {
+  name: 'App',
+  components: {
+    Title,
+    Button
+  },
+  data() {
+    return {
+      text:"Vue 3 Denemesi",
+      text2:"Hello Vue!"
+    }
+  },
+  methods: {
+    cleanInput(){
+      this.$refs.inputText.value = ''
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

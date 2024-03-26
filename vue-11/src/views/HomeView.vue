18d9f25b349154ac82ad642a953249ec06272d3b
Alperen <138169703+NAlpN@users.noreply.github.com>
2024-03-26 09:31:33 +0300

    VueJS ile compositionAPI uygulaması

---
+++ vue-11/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <Contents :contents="contents"></Contents>
+  </div>
+</template>
+
+<script>
+import { ref, computed, watch, watchEffect } from "vue";
+import Contents from "../components/Contents.vue";
+export default {
+  name: "HomeView",
+  components: { Contents },
+  setup() {
+    const contents = ref([
+      { id: 1, title: "Deneme 1", details: "Detail 0" },
+      { id: 2, title: "Deneme 2", details: "Detail 00" },
+      { id: 3, title: "Deneme 3", details: "Detail 000" },
+      { id: 4, title: "Deneme 4", details: "Detail 0000" },
+    ]);
+    return { contents };
+  },
+};
+</script>
